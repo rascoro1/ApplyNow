22397e8e5be47f400281899024de21ed126b313f
rascoro1 <copelanda1@wit.edu>
2016-07-26 17:09:21 -0400

    index.html changed to become index.php
    Cleaned up html with php files

--- php/top_nav.php
+++ php/top_nav.php
@@ -1,8 +1,28 @@
 <?php
-include 'css/bootstrap.min.css';
-include 'css/custom.css';
-include 'css/styles.css';
-echo '<!-- Begin Top Nav Bar -->';
 echo '<header class="navbar navbar-default navbar-static-top" role="banner">';
-echo '<div class="containera">';
+  echo '<div class="container">';
+    echo '<div class="navbar-header">';
+      echo '<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">';
+        echo '<span class="sr-only">Toggle navigation</span>';
+        echo '<span class="icon-bar"></span>';
+        echo '<span class="icon-bar"></span>';
+        echo '<span class="icon-bar"></span>';
+      echo '</button>';
+      echo '<a href="index.html" class="navbar-brand">ApplyNow</a>';
+    echo '</div>';
+    echo '<nav class="collapse navbar-collapse" role="navigation">';
+      echo '<ul class="nav navbar-nav">';
+        echo '<li>';
+          echo '<a href="upload_application.html">Upload Application</a>';
+        echo '</li>';
+        echo '<li>';
+          echo '<a href="register.html">Register</a>';
+        echo '</li>';
+        echo '<li>';
+          echo '<a href="login.html">Login</a>';
+        echo '</li>';
+      echo '</ul>';
+    echo '</nav>';
+  echo '</div>';
+echo '</header>';
 ?>
